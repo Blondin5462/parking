186b2709eeb04c21443a762468b79a0264e44aa2
Blondin5462 <maxime.leszczynski@gmail.com>
2021-11-27 11:14:11 +0100

    Ajout du controller

---
+++ parking/src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -0,0 +1,14 @@
+package com.formation.parking.controllers;
+
+import com.formation.parking.models.Parking;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParkingController {
+
+    @RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
+    public List<Parking> getListeParkings() {
+        return null;
+    }
+}
