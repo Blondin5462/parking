ce127dfcb7f09532bab14ea0f16f0d080521894c
Blondin5462 <maxime.leszczynski@gmail.com>
2021-11-27 11:34:02 +0100

    Création du controller REST

--- parking/src/main/java/com/formation/parking/controllers/ParkingController.java
+++ parking/src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ParkingController {
 
     @RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
     public List<Parking> getListeParkings() {
-        return null;
+        Parking parkingTest = new Parking();
+        parkingTest.setNom("Parking de test");
+        parkingTest.setNbPlacesTotal(300);
+        parkingTest.setNbPlacesDispo(125);
+        parkingTest.setStatut("ouvert");
+        parkingTest.setHeureMaj("20h15");
+        ArrayList<Parking> liste = new ArrayList<>();
+        liste.add(parkingTest);
+        return liste;
     }
 }
