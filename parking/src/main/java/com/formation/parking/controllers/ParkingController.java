bf545fa36594824402bd439aa7e6da0e0564513c
Blondin5462 <maxime.leszczynski@gmail.com>
2021-11-27 11:17:13 +0100

    Ajout du controller(MAJ)

--- parking/src/main/java/com/formation/parking/controllers/ParkingController.java
+++ parking/src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -2,6 +2,7 @@ package com.formation.parking.controllers;
 
 import com.formation.parking.models.Parking;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
